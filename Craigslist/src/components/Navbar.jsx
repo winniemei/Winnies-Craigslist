24c99617b3b4724b675f7a7a68a17d9a0744235f
winniemei <wmei91@gmail.com>
2023-08-18 15:10:59 -0400

    display posts

--- Craigslist/src/components/Navbar.jsx
+++ Craigslist/src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import {Routes, Route, Link} from "react-router-dom";
 export default function NavBar() {
   return (
     <div id="navbar">
-      <Link to="/">HOME</Link>
       <Link to="/register">REGISTER</Link>
       <Link to="/login">LOG IN</Link>
       <Link to="/posts">POSTS</Link>
--- Craigslist/src/components/Posts.jsx
+++ Craigslist/src/components/Posts.jsx
@@ -1,7 +1,41 @@
+import { useState, useEffect } from 'react'
+
+const COHORT_NAME = '2306-ghp-et-web-ft-sf'
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
+
+
 export default function Posts() {
-    return(
-        <>
-        <h1>POSTS</h1>       
-        </>
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        async function fetchAllPosts() {
+            try {
+                const response = await fetch(`${BASE_URL}/posts`);
+                const result = await response.json();
+                console.log(result)
+                setPosts(result.data.posts)
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchAllPosts();
+    }, [])
+    return (
+        <div>
+            <h1>ALL POSTS</h1>
+            <div>
+                {
+                    posts.map((post) => {
+                        return (
+                            <div>
+                                <h4>Username: {post.author.username}</h4>
+                                {/* <h4>Breed: {player.breed}</h4>
+                                <img src={player.imageUrl} alt={player.name} />
+                                <Link to={`/SinglePlayer/${player.id}`}>See Details</Link> */}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
 }
