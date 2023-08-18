c16027f449344bc6d6b16ce07efcc1a9e35a3c7a
winniemei <wmei91@gmail.com>
2023-08-18 13:39:59 -0400

    added routing

--- Craigslist/src/App.jsx
+++ Craigslist/src/App.jsx
@@ -7,6 +7,11 @@ export default function App() {
   return (
     <>
       <h1>HELLO</h1>
+      <Routes>
+        <Route path='register'></Route>
+        <Route path='login'></Route>
+        <Route path='posts'></Route>
+      </Routes>
     </>
   )
 }
