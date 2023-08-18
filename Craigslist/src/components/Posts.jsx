8fd1aafb9f07b7993cb79e1c1acbe3f80385eb80
winniemei <wmei91@gmail.com>
2023-08-18 15:33:40 -0400

    add search bar

--- Craigslist/src/components/Posts.jsx
+++ Craigslist/src/components/Posts.jsx
@@ -6,22 +6,40 @@ const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
 
 export default function Posts() {
     const [posts, setPosts] = useState([]);
+    const [searchParam, setSearchParam] = useState("");
     useEffect(() => {
         async function fetchAllPosts() {
             try {
                 const response = await fetch(`${BASE_URL}/posts`);
                 const result = await response.json();
-                console.log(result)
                 setPosts(result.data.posts)
+                console.log(posts)
             } catch (error) {
                 console.error(error);
             }
         }
         fetchAllPosts();
-    }, [])
+    }, []);
+
+    const postsToDisplay = searchParam
+        ? posts.filter((post) =>
+            post.author.username.toLowerCase().includes(searchParam)
+        )
+        : posts;
+
     return (
         <div>
             <h1>ALL POSTS</h1>
+            <div>
+                <label>
+                    Search:{" "}
+                    <input
+                        type="text"
+                        placeholder="search"
+                        onChange={(e) => setSearchParam(e.target.value.toLowerCase())}
+                    />
+                </label>
+            </div>
             <div>
                 {
                     posts.map((post) => {
