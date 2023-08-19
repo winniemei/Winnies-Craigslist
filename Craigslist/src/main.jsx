a176a47e46266624fa05a951935cd245138db586
winniemei <wmei91@gmail.com>
2023-08-18 23:46:36 -0400

    configured store and added provider

--- Craigslist/src/main.jsx
+++ Craigslist/src/main.jsx
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { configureStore } from '@reduxjs/toolkit'
+import authenticateReducer from './redux/authenticateSlice.js'
+import { Provider } from 'react-redux'
+
+const store = configureStore({
+  reducer: {
+    account: authenticateReducer,
+ }
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>
 )
