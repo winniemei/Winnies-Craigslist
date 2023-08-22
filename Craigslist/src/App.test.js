3f95964b3758f9230fccb714fad4002bfb96651c
winniemei <wmei91@gmail.com>
2023-08-22 09:06:35 -0400

    modified test

--- Craigslist/src/App.test.js
+++ Craigslist/src/App.test.js
@@ -7,12 +7,15 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Posts from './components/Posts'
+import PostName from './components/PostName';
+import CreatePost from './components/CreatePost';
+import Register from './components/Register';
 
 // Unit test/Behavior Test
 describe("rendering", () => {
-    test("renders the App component", () => {
-        render(<Posts />)
-        expect(screen.getByText("ALL POSTS")).toBeInTheDocument()     })
+    test("renders the register component", () => {
+        render(<Register />)
+        expect(screen.getByText("Sign up")).toBeInTheDocument()     })
     // Test: renders the Button component with correct props
 
 })
